586b25e1149fb65b477c5f8c56cb0b6d1dacea42
alenaAG <guzharina.alena@gmail.com>
2022-03-21 12:37:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.header("My first app!!!!!")
+st.header("Oh myyyy!!!!!!!!!")
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
